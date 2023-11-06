3f199478df00596995c508d1c671ab0727e4ffe3
Haitham Alnajar <65522233+infinitly-irrational@users.noreply.github.com>
2023-11-06 13:29:26 -0500

    Initial Commit

---
+++ FactFlict/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FactFlict
+//
+//  Created by Haitham Alnajar on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
